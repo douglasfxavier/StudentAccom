f49be9f6e9c6ce5c1a018e798f92bd91e6b42779
douglasfxavier <douglasf.xavier@gmail.com>
2017-10-26 23:26:05 +0100

    Adding model classes: User and Role

---
+++ StudentAccom/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAccom.Models {
+    public class Role {
+        public int id { set; get; }
+        public string description { set; get; }
+    }
+}
---
+++ StudentAccom/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAccom.Models {
+    public class User {
+        public int îd { get; set; }
+        public string name { get; set; }
+        public Role role { get; set; }
+    }
+}
