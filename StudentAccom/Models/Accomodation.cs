f8aaeba528f3cb55080076325d22ad139e41dd6d
douglasfxavier <douglasf.xavier@gmail.com>
2017-10-30 03:18:29 +0000

    Removing folder .vs

---
+++ StudentAccom/Models/Accomodation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAccom.Models {
+    public class Accomodation {
+        public int id { set; get; }
+        public string description { set; get; }
+        public Byte[] photo { set; get; }
+        public string comment { set; get; }
+        public bool approved { set; get; }
+        public bool rejected { set; get; }
+    }
+}
---
+++ StudentAccom/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAccom.Models {
+    public class Login {
+        public User user;
+        public string login { set; get; }
+        public string password { set; get; }
+    }
+}
