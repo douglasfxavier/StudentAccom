9a6b4faef48a2b17f223ea3ba0fc664f67139cff
huanglongqiuyu <huanglongqiuyu@hotmail.com>
2017-10-30 15:23:31 +0000

    Adding the annotation in the class Accommodation

--- StudentAccom/Models/Accomodation.cs
+++ StudentAccom/Models/Accomodation.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace StudentAccom.Models {
-    public class Accomodation {
+namespace StudentAccom.Models
+{
+    public class Accomodation
+    {
+        [HiddenInput(DisplayValue = false)]
+        [Key]
         public int ID { set; get; }
+        [Required]
         public string Description { set; get; }
         public Byte[] Photo { set; get; }
         public string Comment { set; get; }
