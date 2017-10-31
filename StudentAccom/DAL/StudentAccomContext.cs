a2061869958e71261edf5618508969d9fcecbda7
douglasfxavier <douglasf.xavier@gmail.com>
2017-10-31 14:27:34 +0000

    Adding DAL to the project

---
+++ StudentAccom/DAL/StudentAccomContext.cs
@@ -0,0 +1,14 @@
+﻿using StudentAccom.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentAccom.DAL {
+
+    public class StudentAccomContext : DbContext {
+        public StudentAccomContext() : base("StudentAccomDB") { }
+        public DbSet<User> UsersDB { set; get; }
+    }
+}
