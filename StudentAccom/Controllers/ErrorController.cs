ec6d1a20c95c5443c8fdb0b6840dd0dafea45e1d
douglasfxavier <douglasf.xavier@gmail.com>
2017-11-23 01:58:29 +0000

    Adding Errors handling controller and views

---
+++ StudentAccom/Controllers/ErrorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentAccom.Controllers
+{
+    public class ErrorController : Controller {
+
+        public ActionResult FileUploadLimitExceeded() {
+            return View();
+        }
+
+        public ActionResult BadRequest() {
+            Response.StatusCode = 400;
+
+            return View();
+        }
+
+        public ActionResult PageUnauthorized() {
+            Response.StatusCode = 403;
+
+            return View();
+        }
+
+        public ActionResult PageNotFound() {
+            Response.StatusCode = 404;
+            return View();
+        }
+        
+        public ActionResult InternalServerError() {
+            Response.StatusCode = 500;
+
+            return View();
+         }
+    }
+}
