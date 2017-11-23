61f61b06d88799a81d473d87a0870c2f9a985049
douglasfxavier <douglasf.xavier@gmail.com>
2017-11-23 02:56:55 +0000

    Adding search box

--- StudentAccom/Controllers/AdvertisementsController.cs
+++ StudentAccom/Controllers/AdvertisementsController.cs
@@ -29,24 +29,28 @@ namespace StudentAccom.Controllers
         [Authorize(Roles = "Admin, AccommodationOfficer, Landlord")]
         [Route("Advertisements/List")]
         [HttpGet]
-        public ActionResult List() {
+        public ActionResult List(string LandlordID, string search) {
             DBContext = new StudentAccomContext();
             IdentityContext = new ApplicationDbContext();
 
-            IEnumerable<Accommodation> accommodations;
+            IEnumerable<Accommodation> accommodations = DBContext.AccommodationsDB;
+
+            if (!String.IsNullOrEmpty(LandlordID)) {
+                accommodations = accommodations.Where(a => a.LandlordID == LandlordID);
+            }
+
+            if (!String.IsNullOrEmpty(search)) {
+                accommodations = accommodations.Where(a => a.Title.Contains(search) || a.Description.Contains(search) || a.Location.Contains(search));
+            }
 
             if (User.IsInRole("Landlord")) {
                 var userId = User.Identity.GetUserId();
                 //Filter by Landlord User based on its ID
-                var accomFilter = DBContext.AccommodationsDB.Where(a => a.LandlordID.Equals(userId));
-                accommodations = accomFilter.ToArray();
-            } else {
-                //All accommodation records
-                accommodations = DBContext.AccommodationsDB.ToArray();
-            }
+                accommodations = accommodations.Where(a => a.LandlordID.Equals(userId));
+            } 
 
             //Get the name (or campany name) from users and create a map wich key is the ID. The aim is to avoid accecces to the password column
-            var landlords = IdentityContext.Users.ToArray();
+            var landlords = IdentityContext.Users;
             var map = new Dictionary<string, string>();
 
             foreach (var l in landlords) {
@@ -58,7 +62,7 @@ namespace StudentAccom.Controllers
                 
             }
             ViewData.Add("Landlords", map);
-            return View(accommodations);
+            return View(accommodations.ToArray());
         }
 
 
