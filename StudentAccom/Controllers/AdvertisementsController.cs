5c9c8c21d8b69eb5395989bc39817aedec21f8c5
douglasfxavier <douglasf.xavier@gmail.com>
2017-11-23 00:13:07 +0000

    Adding column Landlord/Company on the view Advertisements/List

--- StudentAccom/Controllers/AdvertisementsController.cs
+++ StudentAccom/Controllers/AdvertisementsController.cs
@@ -12,6 +12,7 @@ namespace StudentAccom.Controllers
     public class AdvertisementsController : Controller
     {
         private StudentAccomContext DBContext;
+        private ApplicationDbContext IdentityContext;
 
         // GET: Main
         [Route("Advertisements/Show")]
@@ -30,7 +31,10 @@ namespace StudentAccom.Controllers
         [HttpGet]
         public ActionResult List() {
             DBContext = new StudentAccomContext();
+            IdentityContext = new ApplicationDbContext();
+
             IEnumerable<Accommodation> accommodations;
+
             if (User.IsInRole("Landlord")) {
                 var userId = User.Identity.GetUserId();
                 //Filter by Landlord User based on its ID
@@ -40,6 +44,20 @@ namespace StudentAccom.Controllers
                 //All accommodation records
                 accommodations = DBContext.AccommodationsDB.ToArray();
             }
+
+            //Get the name (or campany name) from users and create a map wich key is the ID. The aim is to avoid accecces to the password column
+            var landlords = IdentityContext.Users.ToArray();
+            var map = new Dictionary<string, string>();
+
+            foreach (var l in landlords) {
+                if (l.Company != null && l.Company.Length > 0) {
+                    map.Add(l.Id, l.Company);
+                } else {
+                    map.Add(l.Id, string.Format("{0} {1}", l.FirstName, l.LastName));
+                }
+                
+            }
+            ViewData.Add("Landlords", map);
             return View(accommodations);
         }
 
